21578f64bf45543f98acbf4316257b2176e78a77
Paul Panchukov <panchukovp@gmail.com>
2019-10-12 03:15:26 -0400

    updated layout responsiveness

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,34 +1,62 @@
 $("#aboutMeBtn").on("click", function(){
-    window.scrollTo({
-        top: 850,
-        behavior: "smooth"
-    })
+    if (window.innerWidth >= 1670) {
+        window.scrollTo({
+            top: 850,
+            behavior: "smooth"
+        })
+    }else if (window.innerWidth >= 900) {
+            window.scrollTo({
+                top:850,
+                behavior: "smooth"
+            })
+    }
 })
 $("#portfolioBtn").on("click", function(){
-    window.scrollTo({
-        top: 1710,
-        behavior: "smooth"
-    })
+    if (window.innerWidth >= 1670) {
+        window.scrollTo({
+            top: 1715,
+            behavior: "smooth"
+        })
+    }else if (window.innerWidth >= 900) {
+        window.scrollTo({
+            top:1715,
+            behavior: "smooth"
+        })
+}
 })
 $("#contactMeBtn").on("click", function(){
+    if (window.innerWidth >= 1670) {
     window.scrollTo({
         top: 2600,
         behavior: "smooth"
     })
+}else if (window.innerWidth >= 900) {
+    window.scrollTo({
+        top:2600,
+        behavior: "smooth"
+    })
+}
 })
 $("#homeBtn").on("click", function(){
+
     window.scrollTo({
         top: 0,
         behavior: "auto"
     })
+
 })
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("motto").style.top = "0";
-  } else {
-    document.getElementById("motto").style.top = "-166px";
+  } else {if (window.innerWidth >= 1330 && window.innerHeight <= 10) {
+    document.getElementById("motto").style.top = "-168px";
+}else if (window.innerWidth >= 864) {
+    document.getElementById("motto").style.top = "-360px";
+}else if (window.innerWidth >= 768) {
+    document.getElementById("motto").style.top = "-490px";
+}
   }
   prevScrollpos = currentScrollPos;
 }
